861e3fedc968a7bce148cd5eff0177ceb5e3c05f
ABoba <a.boba@yandex.ru>
2024-01-23 10:08:38 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Math.Pow((x+1/x-1),2) + 18*x*Math.Pow(y,2));
+        }
+    }
+}
